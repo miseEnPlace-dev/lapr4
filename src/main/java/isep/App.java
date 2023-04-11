5c83c04d97d03b0d655a916f53c1fde8e2bd503b
Tomás Russo <tomas.russo4@gmail.com>
2023-04-11 17:47:38 +0100

    chore: updated editorconfig - indent_size from 4 to 2

--- src/main/java/isep/App.java
+++ src/main/java/isep/App.java
@@ -4,12 +4,12 @@ import java.sql.SQLException;
 import org.h2.tools.Server;
 
 public class App {
-    public static void main(String[] args) throws SQLException {
-        startDatabase();
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) throws SQLException {
+    startDatabase();
+    System.out.println("Hello World!");
+  }
 
-    private static void startDatabase() throws SQLException {
-        new Server().runTool("-tcp", "-web", "-ifNotExists");
-    }
+  private static void startDatabase() throws SQLException {
+    new Server().runTool("-tcp", "-web", "-ifNotExists");
+  }
 }
