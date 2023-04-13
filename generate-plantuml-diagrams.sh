21fcd81fe86723e4a5cd14a85cb9336f20519711
Tomás Lopes <tomas050302@gmail.com>
2023-04-13 09:25:35 +0100

    chore: updated generate plant uml script to use out folder

--- generate-plantuml-diagrams.sh
+++ generate-plantuml-diagrams.sh
@@ -4,12 +4,16 @@ exportFormat="svg"
 #monochrome="true"
 extra="-SdefaultFontSize=20"
 #extra="-SdefaultFontName=Times New Roman -SdefaultFontSize=10"
+out=out
 
 for aFile in `find docs -name "*.puml" -type f`;
 do
   #-Smonochrome=$monochrome
   echo "Processing file: $aFile"
-	java -jar libs/plantuml-1.2023.1.jar $extra -t$exportFormat $aFile 
+  file=$(echo $aFile | cut -d "/" -f 3)
+  echo "Output file: ./$out/$file.$exportFormat"
+
+	java -jar libs/plantuml-1.2023.1.jar $extra -t$exportFormat $aFile -o$out
 done
 
 echo "Finished"
