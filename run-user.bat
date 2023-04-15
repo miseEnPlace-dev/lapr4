15a9cc33eb166dc60683ace9fc8758b59a3b9bce
Ricardo M <rmoreiradev@gmail.com>
2023-04-15 16:30:38 +0100

    fix: run user app windows script

--- run-user.bat
+++ run-user.bat
@@ -3,4 +3,4 @@ REM assumes the build was executed with maven copy-dependencies
 SET ECOURSE_CP=ecourse.app.user.console\target\app.user.console-1.0.0.jar;ecourse.app.user.console\target\dependency\*;
 
 REM call the java VM, e.g,
-java -cp %ECOURSE_CP% eapli.ecourse.app.user.console.BaseUserApp
+java -cp %ECOURSE_CP% eapli.ecourse.app.user.console.ECourseUserApp
