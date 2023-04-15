ca295d0376f05b28f91d102123b886f007e69851
Ricardo M <rmoreiradev@gmail.com>
2023-04-15 16:26:06 +0100

    chore: updated h2 script to allow db creation

--- h2.bat
+++ h2.bat
@@ -1,2 +1,3 @@
-@java -cp "libs\h2-2.1.214.jar;%H2DRIVERS%;%CLASSPATH%" org.h2.tools.Console %*
+@java -cp "libs\h2-2.1.214.jar;%H2DRIVERS%;%CLASSPATH%" org.h2.tools.Server -tcp -web -ifNotExists
+REM org.h2.tools.Console %*
 @if errorlevel 1 pause
--- h2.sh
+++ h2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 dir=$(dirname "$0")
-java -cp "$dir/libs/h2-2.1.214.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"
+java -cp "$dir/libs/h2-2.1.214.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Server -tcp -web -ifNotExists
