a8feb4a62ff6856dbcf93d8c60cd6be96a8d9454
Ricardo M <rmoreiradev@gmail.com>
2023-04-21 10:15:03 +0100

    domain(#27): added `Class` domain classes

---
+++ ecourse.core/src/main/java/eapli/ecourse/classmanagement/domain/Duration.java
@@ -0,0 +1,38 @@
+package eapli.ecourse.classmanagement.domain;
+
+import javax.persistence.Embeddable;
+import eapli.framework.domain.model.ValueObject;
+import lombok.EqualsAndHashCode;
+
+@Embeddable
+@EqualsAndHashCode
+public class Duration implements ValueObject, Comparable<Duration> {
+  private static final long serialVersionUID = 1L;
+
+  private int duration;
+
+  protected Duration(final int duration) {
+    if (duration <= 0)
+      throw new IllegalArgumentException("Duration should be > 0");
+
+    this.duration = duration;
+  }
+
+  protected Duration() {
+    // for ORM
+  }
+
+  public static Duration valueOf(final int duration) {
+    return new Duration(duration);
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(this.duration);
+  }
+
+  @Override
+  public int compareTo(final Duration arg0) {
+    return duration - arg0.duration;
+  }
+}
---
+++ ecourse.core/src/main/java/eapli/ecourse/classmanagement/domain/WeekDay.java
@@ -0,0 +1,5 @@
+package eapli.ecourse.classmanagement.domain;
+
+public enum WeekDay {
+  SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
