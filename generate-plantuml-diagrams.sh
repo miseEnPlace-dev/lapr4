26d298641927d3e7d57a554ce8dce9956f865ac8
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-04 22:22:42 +0100

    chore: updated generate plant uml script to delete old out folder

--- generate-plantuml-diagrams.sh
+++ generate-plantuml-diagrams.sh
@@ -6,12 +6,22 @@ extra="-SdefaultFontSize=20"
 #extra="-SdefaultFontName=Times New Roman -SdefaultFontSize=10"
 out=out
 
+for out_dir in `find docs -name "out" -type d`;
+do
+  echo "> Removing old output files: $out_dir"
+  rm -rf $out_dir
+done
+
+echo
+echo "> Generating files..."
+echo
+
 for aFile in `find docs -name "*.puml" -type f`;
 do
   #-Smonochrome=$monochrome
-  echo "Processing file: $aFile"
+  echo "> Processing file: $aFile"
   file=$(echo $aFile | cut -d "/" -f 3)
-  echo "Output file: ./$out/$file.$exportFormat"
+  echo " >> Output file: ./$out/$file.$exportFormat"
 
 	java -jar libs/plantuml-1.2023.1.jar $extra -t$exportFormat $aFile -o$out
 done
