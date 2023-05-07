c0209dd7c87e476df1a47116d8c933b0ed5158eb
Gonçalo Lima <1211350@isep.ipp.pt>
2023-05-07 02:33:12 +0100

    feat: updated Controller (#34)

--- ecourse.core/src/main/java/eapli/ecourse/coursemanagement/application/ToggleCourseStatusController.java
+++ ecourse.core/src/main/java/eapli/ecourse/coursemanagement/application/ToggleCourseStatusController.java
@@ -3,15 +3,21 @@ package eapli.ecourse.coursemanagement.application;
 import eapli.ecourse.coursemanagement.domain.Course;
 import eapli.ecourse.coursemanagement.domain.CourseState;
 import eapli.ecourse.coursemanagement.repositories.CourseRepository;
+import eapli.ecourse.infrastructure.persistence.PersistenceContext;
+import eapli.ecourse.infrastructure.persistence.RepositoryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 
 public class ToggleCourseStatusController {
+
+  private final RepositoryFactory repositoryFactory;
 
   @Autowired
   private CourseRepository courseRepository;
 
+
   public ToggleCourseStatusController() {
-    
+    repositoryFactory = PersistenceContext.repositories();
   }
 
   public Iterable<Course> listOpenCourses() {
