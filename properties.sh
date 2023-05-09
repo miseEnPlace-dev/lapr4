f073afd0706eca1c3e880e8468fc781c756d7631
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-09 12:22:14 +0100

    chore: updated properties script to preserve existing configurations

--- properties.sh
+++ properties.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
+echo
 ls | grep ecourse. | while read line; do
   path="$line/src/main/resources"
   if [ -f "$path/application.example.properties" ]; then
-    cp "$path/application.example.properties" "$path/application.properties"
+    if [ -f "$path/application.properties" ]; then
+      echo "> $path already has application.properties. Skipping..."
+    else
+      echo "> Creating $path/application.properties"
+      cp "$path/application.example.properties" "$path/application.properties"
+    fi
   fi
 done
 
