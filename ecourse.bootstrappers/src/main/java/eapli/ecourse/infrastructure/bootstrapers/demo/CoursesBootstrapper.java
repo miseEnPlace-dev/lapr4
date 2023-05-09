48162ad88f413e33007a3329608141862eaee93a
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-09 21:25:53 +0100

    chore: added course bootstrapper skeleton

---
+++ ecourse.bootstrappers/src/main/java/eapli/ecourse/infrastructure/bootstrapers/demo/CoursesBootstrapper.java
@@ -0,0 +1,17 @@
+package eapli.ecourse.infrastructure.bootstrapers.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import eapli.framework.actions.Action;
+
+public class CoursesBootstrapper implements Action {
+  private static final Logger LOGGER = LogManager.getLogger(CoursesBootstrapper.class);
+
+  @Override
+  public boolean execute() {
+    // TODO implement
+
+    return false;
+  }
+}
