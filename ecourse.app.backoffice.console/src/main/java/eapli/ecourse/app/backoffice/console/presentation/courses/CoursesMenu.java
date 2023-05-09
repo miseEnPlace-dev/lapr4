d4c523e71ded16a5de717f2795a22496e2e0852e
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-09 20:02:19 +0100

    fix: fixed const declaration error

--- ecourse.app.backoffice.console/src/main/java/eapli/ecourse/app/backoffice/console/presentation/courses/CoursesMenu.java
+++ ecourse.app.backoffice.console/src/main/java/eapli/ecourse/app/backoffice/console/presentation/courses/CoursesMenu.java
@@ -5,14 +5,14 @@ import eapli.framework.actions.menu.Menu;
 import eapli.framework.presentation.console.ShowMessageAction;
 
 public class CoursesMenu {
+  private static final int LIST_COURSES_OPTION = 1;
+  private static final int TOGGLE_COURSE_STATE_OPTION = 2;
+  private static final int TOGGLE_COURSE_ENROLMENT_STATE_OPTION = 3;
+  private static final int EXIT_OPTION = 0;
+
+  private static final String RETURN_LABEL = "Return ";
+
   public Menu buildCoursesMenu() {
-    private static final int LIST_COURSES_OPTION = 1;
-    private static final int TOGGLE_COURSE_STATE_OPTION = 2;
-    private static final int TOGGLE_COURSE_ENROLMENT_STATE_OPTION = 3;
-    private static final int EXIT_OPTION = 0;
-
-    private static final String RETURN_LABEL = "Return ";
-
     final Menu menu = new Menu("Courses >");
 
     menu.addItem(LIST_COURSES_OPTION, "List All Courses", new ShowMessageAction("Not implemented yet"));
