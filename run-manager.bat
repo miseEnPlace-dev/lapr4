31572ff596a47b3b85feb1710c36061d29e9bcef
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-12 15:56:30 +0100

    chore: removed manager app

--- run-manager.bat
+++
@@ -1,6 +0,0 @@
-REM set the class path,
-REM assumes the build was executed with maven copy-dependencies
-SET ECOURSE_CP=ecourse.app.manager.console\target\app.manager.console-1.0.0.jar;ecourse.app.manager.console\target\dependency\*;
-
-REM call the java VM, e.g,
-java -cp %ECOURSE_CP% eapli.ecourse.app.manager.console.ECourseManagerApp
