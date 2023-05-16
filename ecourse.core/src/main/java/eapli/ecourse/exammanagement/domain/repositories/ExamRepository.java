3ac3e931239836ddb0e83de0334199053bdd47ea
goncalolima2611 <1211350@isep.ipp.pt>
2023-05-16 20:33:28 +0100

    docs: added SSD (#44)

--- ecourse.core/src/main/java/eapli/ecourse/exammanagement/domain/repositories/ExamRepository.java
+++ ecourse.core/src/main/java/eapli/ecourse/exammanagement/domain/repositories/ExamRepository.java
@@ -7,11 +7,22 @@ import eapli.framework.domain.repositories.DomainRepository;
 import java.util.Optional;
 
 public interface ExamRepository extends DomainRepository<Long, Exam> {
-
+  /**
+   * Returns the exam with the given id.
+   *
+   * @param id
+   * @return
+   */
   default Optional<Exam> findById(final Long id) {
     return ofIdentity(id);
   }
 
+  /**
+   * Returns the exams of the given course.
+   *
+   * @param course
+   * @return
+   */
   Iterable<Exam> findAllCourseExams(Course course);
 
 }
