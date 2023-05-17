9cde2cea904b4a3377d53f0b8b5bf73d1f5c0a98
André Barros <1211299@isep.ipp.pt>
2023-05-17 18:33:46 +0100

    chore: moved system user printer

--- ecourse.app.common.console/src/main/java/eapli/ecourse/app/common/console/presentation/meeting/ScheduleMeetingUI.java
+++ ecourse.app.common.console/src/main/java/eapli/ecourse/app/common/console/presentation/meeting/ScheduleMeetingUI.java
@@ -1,20 +1,80 @@
 package eapli.ecourse.app.common.console.presentation.meeting;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import eapli.ecourse.app.common.console.presentation.authz.SystemUserPrinter;
+import eapli.ecourse.eventsmanagement.domain.Duration;
+import eapli.ecourse.eventsmanagement.domain.Time;
 import eapli.ecourse.eventsmanagement.meetingmanagement.application.ScheduleMeetingController;
 import eapli.ecourse.infrastructure.persistence.PersistenceContext;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.application.UserManagementService;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.SelectWidget;
 
 public class ScheduleMeetingUI extends AbstractUI {
 
-  private final ScheduleMeetingController theController = new ScheduleMeetingController(null,
-      AuthzRegistry.authorizationService());
+  private UserManagementService userManagementService = AuthzRegistry.userService();
+
+  private final ScheduleMeetingController ctrl = new ScheduleMeetingController(
+      PersistenceContext.repositories().meetings(),
+      AuthzRegistry.authorizationService(), PersistenceContext.repositories().invites());
 
   @Override
   protected boolean doShow() {
-    System.out.println("Schedule Meeting: ");
+    Calendar time = Console.readCalendar("Time (HH:MM): ", "HH:mm");
+    Time meetingTime = Time.valueOf(time);
 
-    return false;
+    Integer duration = Console.readInteger("Duration (minutes): ");
+    Duration meetingDuration = Duration.valueOf(duration);
+
+    List<SystemUser> allUsers = (ArrayList<SystemUser>) userManagementService.allUsers();
+    Set<SystemUser> selectedUsers = new HashSet<>();
+
+    if (!allUsers.iterator().hasNext()) {
+      System.out.println("There are no registered users.");
+      return false;
+    }
+
+    final SelectWidget<SystemUser> selector = new SelectWidget<>("\nUsers:", allUsers, new SystemUserPrinter());
+
+    while (selector != null && allUsers.size() > 0) {
+      selector.show();
+      final SystemUser selected = selector.selectedElement();
+      allUsers.remove(selected);
+      selectedUsers.add(selected);
+    }
+
+    System.out.println("\nDo you want to submit the data? [Y/N]");
+
+    String confirm = "";
+    while (!confirm.equals("Y") && !confirm.equals("N")) {
+      confirm = Console.readLine("Option: ").toUpperCase();
+    }
+
+    if (confirm.equals("N")) {
+      System.out.println("Operation Cancelled!");
+      return false;
+    }
+
+    try {
+      ctrl.scheduleMeeting(meetingTime, meetingDuration, selectedUsers);
+    } catch (IllegalStateException e) {
+      System.out.println(e.getMessage());
+      System.out.println("Error creating meeting.");
+      return false;
+    }
+
+    System.out.println("\nMeeting successfully created");
+    Console.readLine("Press Enter to continue...");
+
+    return true;
   }
 
   @Override
