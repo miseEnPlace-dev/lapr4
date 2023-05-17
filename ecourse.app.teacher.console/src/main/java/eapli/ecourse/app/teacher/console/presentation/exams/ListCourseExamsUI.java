6dec93322a4e94af1f8ac0ba91aa8b6963f4bc07
goncalolima2611 <1211350@isep.ipp.pt>
2023-05-17 01:04:41 +0100

    feat: added UI (#44)

--- ecourse.app.teacher.console/src/main/java/eapli/ecourse/app/teacher/console/presentation/exams/ListCourseExamsUI.java
+++ ecourse.app.teacher.console/src/main/java/eapli/ecourse/app/teacher/console/presentation/exams/ListCourseExamsUI.java
@@ -1,2 +1,62 @@
-package eapli.ecourse.app.teacher.console.presentation.exams;public class ListCourseExamsUI {
+package eapli.ecourse.app.teacher.console.presentation.exams;
+
+
+import eapli.ecourse.app.common.console.presentation.course.CoursePrinter;
+import eapli.ecourse.app.common.console.presentation.exam.ExamPrinter;
+import eapli.ecourse.coursemanagement.application.ListCourseExamsController;
+import eapli.ecourse.coursemanagement.dto.CourseDTO;
+import eapli.ecourse.coursemanagement.repositories.CourseRepository;
+import eapli.ecourse.exammanagement.domain.dto.ExamDTO;
+import eapli.ecourse.exammanagement.domain.repositories.ExamRepository;
+import eapli.ecourse.infrastructure.persistence.PersistenceContext;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.ListWidget;
+import eapli.framework.presentation.console.SelectWidget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListCourseExamsUI extends AbstractUI {
+
+  private final CourseRepository courseRepository = PersistenceContext.repositories().courses();
+
+  private final ExamRepository examRepository = PersistenceContext.repositories().exams();
+  private final ListCourseExamsController ctrl = new ListCourseExamsController
+    (AuthzRegistry.authorizationService(), courseRepository, examRepository);
+
+  @Override
+  protected boolean doShow() {
+    Iterable<CourseDTO> courses = ctrl.listOpenInProgressCourses();
+    List<CourseDTO> courseList = new ArrayList<>();
+    courses.forEach(courseList::add);
+
+    if (courseList.isEmpty()) {
+      System.out.println("There are no courses available.");
+      return false;
+    }
+
+    final SelectWidget<CourseDTO> selector = new SelectWidget<>("Courses:", courseList, new CoursePrinter());
+    selector.show();
+    final CourseDTO selected = selector.selectedElement();
+
+    Iterable<ExamDTO> exams;
+    try {
+      exams = ctrl.listCourseExams(selected);
+    } catch (IllegalArgumentException exception) {
+      System.out.println("There is no course with the given code");
+      return false;
+    }
+    System.out.println(exams.iterator().hasNext());
+
+    ListWidget<ExamDTO> list = new ListWidget<>("Exams of "+selected.getTitle(), exams, new ExamPrinter());
+    list.show();
+
+    return true;
+  }
+
+  @Override
+  public String headline() {
+    return "List Exams In Course";
+  }
 }
