991163bd49baa8e9d6423c950eb85b396c428c29
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-19 19:02:04 +0100

    test: added course title & enrolment state tests

---
+++ ecourse.core/src/test/java/eapli/ecourse/coursemanagement/domain/CourseEnrolmentStateTest.java
@@ -0,0 +1,37 @@
+package eapli.ecourse.coursemanagement.domain;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import eapli.ecourse.coursemanagement.domain.CourseEnrolmentState.EnrolmentState;
+
+public class CourseEnrolmentStateTest {
+  @Test
+  public void ensureCourseEnrolmentStateIsCreatedInClosedState() {
+    CourseEnrolmentState state = new CourseEnrolmentState();
+    assertTrue(state.isClosed());
+  }
+
+  @Test
+  public void ensureItIsPossibleToToggle() {
+    CourseEnrolmentState state = new CourseEnrolmentState();
+    assertTrue(state.isClosed());
+    state.toggle();
+    assertTrue(state.isOpen());
+  }
+
+  @Test
+  public void ensureSameAsWorks() {
+    CourseEnrolmentState state = new CourseEnrolmentState();
+    assertTrue(state.isSameAs(EnrolmentState.CLOSED));
+  }
+
+  @Test
+  public void ensureToStringWorks() {
+    CourseEnrolmentState state = new CourseEnrolmentState();
+    assertTrue(state.toString().equals("CLOSED"));
+    state.toggle();
+    assertTrue(state.toString().equals("OPEN"));
+  }
+}
