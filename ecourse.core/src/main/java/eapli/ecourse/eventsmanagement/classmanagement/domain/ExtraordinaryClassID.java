69bf2637ff36bf99fc3cec2d88684ad14ef329d4
André Barros <1211299@isep.ipp.pt>
2023-05-19 12:12:55 +0100

    fix: extraordinary class domain (#49)

---
+++ ecourse.core/src/main/java/eapli/ecourse/eventsmanagement/classmanagement/domain/ExtraordinaryClassID.java
@@ -0,0 +1,41 @@
+package eapli.ecourse.eventsmanagement.classmanagement.domain;
+
+import java.util.UUID;
+
+import eapli.framework.domain.model.ValueObject;
+import eapli.framework.validations.Preconditions;
+
+public class ExtraordinaryClassID implements ValueObject, Comparable<ExtraordinaryClassID> {
+  private static final long serialVersionUID = 1L;
+
+  private String extraordinaryClassId;
+
+  private ExtraordinaryClassID(final String extraordinaryClassId) {
+    Preconditions.nonEmpty(extraordinaryClassId);
+
+    this.extraordinaryClassId = UUID.fromString(extraordinaryClassId).toString();
+  }
+
+  protected ExtraordinaryClassID() {
+    // for ORM
+    this.extraordinaryClassId = null;
+  }
+
+  public static ExtraordinaryClassID valueOf(final String meetingId) {
+    return new ExtraordinaryClassID(meetingId);
+  }
+
+  public static ExtraordinaryClassID newID() {
+    return valueOf(UUID.randomUUID().toString());
+  }
+
+  @Override
+  public String toString() {
+    return this.extraordinaryClassId;
+  }
+
+  @Override
+  public int compareTo(final ExtraordinaryClassID arg0) {
+    return extraordinaryClassId.compareTo(arg0.extraordinaryClassId);
+  }
+}
