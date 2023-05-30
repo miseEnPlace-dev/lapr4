0fb3c8e4848c6b8c364d9a07767883c1c114a813
Gonçalo Lima <1211350@isep.ipp.pt>
2023-05-30 14:01:38 +0100

    docs: added SSD (#59)

--- ecourse.persistence.impl/src/main/java/eapli/ecourse/persistence/impl/inmemory/InMemoryExamAnswerRepository.java
+++ ecourse.persistence.impl/src/main/java/eapli/ecourse/persistence/impl/inmemory/InMemoryExamAnswerRepository.java
@@ -4,6 +4,7 @@ import eapli.ecourse.answermanagement.domain.ExamAnswer;
 import eapli.ecourse.answermanagement.domain.ExamAnswerId;
 import eapli.ecourse.answermanagement.repositories.ExamAnswerRepository;
 import eapli.ecourse.coursemanagement.domain.CourseCode;
+import eapli.ecourse.exammanagement.domain.Exam;
 import eapli.ecourse.studentmanagement.domain.MecanographicNumber;
 import eapli.framework.infrastructure.repositories.impl.inmemory.InMemoryDomainRepository;
 
@@ -18,5 +19,9 @@ public class InMemoryExamAnswerRepository extends InMemoryDomainRepository<ExamA
   public Iterable<ExamAnswer> findAllWithStudentMecanographicNumberAndCourseCode(MecanographicNumber number,
       CourseCode code) {
     return match(e -> e.student().identity().equals(number) && e.exam().course().code().equals(code));
+  }
+
+  public Iterable<ExamAnswer> findAllWithExam(Exam exam) {
+    return match(e -> e.exam().equals(exam));
   }
 }
