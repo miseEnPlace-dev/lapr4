e5e5ac29b5f8fb94e44410e63d6e0da7dc21af0e
Gonçalo Lima <1211350@isep.ipp.pt>
2023-06-07 19:24:32 +0100

    feat: updated controller (#59)

--- ecourse.core/src/main/java/eapli/ecourse/exammanagement/application/ListCourseExamGradesController.java
+++ ecourse.core/src/main/java/eapli/ecourse/exammanagement/application/ListCourseExamGradesController.java
@@ -4,17 +4,28 @@ import eapli.ecourse.answermanagement.application.ListExamAnswerService;
 import eapli.ecourse.answermanagement.dto.ExamAnswerDTO;
 import eapli.ecourse.answermanagement.repositories.ExamAnswerRepository;
 import eapli.ecourse.coursemanagement.application.ListCourseService;
+import eapli.ecourse.coursemanagement.domain.Course;
 import eapli.ecourse.coursemanagement.dto.CourseDTO;
 import eapli.ecourse.coursemanagement.repositories.CourseRepository;
+import eapli.ecourse.enrolmentmanagement.application.ListEnrolmentService;
+import eapli.ecourse.enrolmentmanagement.dto.EnrolmentDTO;
+import eapli.ecourse.enrolmentmanagement.repositories.EnrolmentRepository;
 import eapli.ecourse.exammanagement.dto.EvaluationExamDTO;
 import eapli.ecourse.exammanagement.dto.FormativeExamDTO;
 import eapli.ecourse.exammanagement.repositories.EvaluationExamRepository;
 import eapli.ecourse.exammanagement.repositories.FormativeExamRepository;
+import eapli.ecourse.studentmanagement.application.StudentService;
+import eapli.ecourse.studentmanagement.domain.Student;
+import eapli.ecourse.studentmanagement.repositories.StudentRepository;
 import eapli.ecourse.teachermanagement.domain.Teacher;
 import eapli.ecourse.teachermanagement.repositories.TeacherRepository;
 import eapli.ecourse.usermanagement.domain.ClientRoles;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+import eapli.framework.infrastructure.authz.domain.model.Username;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class ListCourseExamGradesController {
 
@@ -30,16 +41,23 @@ public class ListCourseExamGradesController {
 
   private final ListExamAnswerService answerService;
 
+  private final ListEnrolmentService enrolmentService;
+
+  private final StudentService studentService;
+
 
   public ListCourseExamGradesController(AuthorizationService authz, EvaluationExamRepository evaluationRepository,
                                         FormativeExamRepository formativeRepository, CourseRepository courseRepository,
-                                        TeacherRepository teacherRepository, ExamAnswerRepository answerRepository) {
+                                        TeacherRepository teacherRepository, ExamAnswerRepository answerRepository,
+                                        EnrolmentRepository enrolmentRepository) {
     this.evaluationService = new EvaluationExamListService(evaluationRepository);
     this.formativeService = new FormativeExamListService(formativeRepository);
     this.authz = authz;
     this.courseService = new ListCourseService(courseRepository);
     this.teacherRepository = teacherRepository;
     this.answerService = new ListExamAnswerService(answerRepository, evaluationRepository, formativeRepository);
+    this.enrolmentService = new ListEnrolmentService(enrolmentRepository);
+    this.studentService = new StudentService();
   }
 
   public Iterable<CourseDTO> teacherCourses() {
@@ -59,11 +77,22 @@ public class ListCourseExamGradesController {
   }
 
   public Iterable<ExamAnswerDTO> evaluationExamGrades(EvaluationExamDTO examDTO) {
-    return answerService.listExamGrades(evaluationService.findExamByCode(examDTO.getCode()).orElseThrow());
+    return answerService.listExamGrades(evaluationService.findExamByCode(examDTO.getCode()).orElseThrow(),
+      studentsInCourse(examDTO.getCourse()));
   }
 
   public Iterable<ExamAnswerDTO> formativeExamGrades(FormativeExamDTO examDTO) {
-    return answerService.listExamGrades(formativeService.findExamByCode(examDTO.getCode()).orElseThrow());
+    return answerService.listExamGrades(formativeService.findExamByCode(examDTO.getCode()).orElseThrow(),
+      studentsInCourse(examDTO.getCourse()));
   }
 
+  private Collection<Student> studentsInCourse(Course course) {
+    Collection<Student> students = new ArrayList<>();
+
+    Iterable<EnrolmentDTO> enrolments = enrolmentService.listStudentsEnrolled(course.code());
+
+    enrolments.forEach(e -> students.add(studentService.findStudentUserByMecNumber(e.getStudentNumber().toString()).orElseThrow()));
+    return students;
+  }
 }
+
