6dca5dc63c3786edc7aa46cf8e742ed3fb75bbae
Ricardo M <rmoreiradev@gmail.com>
2023-06-10 18:46:28 +0100

    feat: postit dto

--- ecourse.core/src/main/java/eapli/ecourse/postitmanagement/application/PostItService.java
+++ ecourse.core/src/main/java/eapli/ecourse/postitmanagement/application/PostItService.java
@@ -7,16 +7,24 @@ import eapli.ecourse.boardmanagement.domain.BoardID;
 import eapli.ecourse.boardmanagement.repositories.BoardRepository;
 import eapli.ecourse.postitmanagement.domain.Coordinates;
 import eapli.ecourse.postitmanagement.domain.PostIt;
+import eapli.ecourse.postitmanagement.domain.PostItID;
+import eapli.ecourse.postitmanagement.dto.PostItDTO;
 import eapli.ecourse.postitmanagement.repositories.PostItRepository;
 
-public class BoardService {
+public class PostItService {
+  private BoardRepository boardRepository;
+  private PostItRepository postItRepository;
 
-  BoardRepository boardRepository;
-  PostItRepository postItRepository;
-
-  public BoardService(BoardRepository boardRepository, PostItRepository postItRepository) {
+  public PostItService(BoardRepository boardRepository, PostItRepository postItRepository) {
     this.boardRepository = boardRepository;
     this.postItRepository = postItRepository;
+  }
+
+  public PostItDTO ofIdentity(PostItID id) {
+    PostIt p = postItRepository.ofIdentity(id).orElseThrow(
+        () -> new IllegalArgumentException("There is no post-it with the given id: " + id));
+
+    return p.toDto();
   }
 
   public boolean existsPostIt(BoardID id, int x, int y) {
