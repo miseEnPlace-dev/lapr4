f8118db6c40289e6bb4f5e8877e7fee2259afe3d
Carlos Lopes <1211277@isep.ipp.pt>
2023-06-10 19:54:36 +0100

    fix: merge conflicts

--- ecourse.persistence.impl/src/main/java/eapli/ecourse/persistence/impl/jpa/JpaBoardRepository.java
+++ ecourse.persistence.impl/src/main/java/eapli/ecourse/persistence/impl/jpa/JpaBoardRepository.java
@@ -42,37 +42,34 @@ public class JpaBoardRepository extends JpaAutoTxRepository<Board, BoardID, Boar
     return query.getResultList();
   }
 
-<<<<<<< HEAD
   public Iterable<Board> findAllActiveBoardsWithUserWritePermission(Username username) {
     TypedQuery<Board> query = createQuery(
         "SELECT b FROM Board b LEFT JOIN b.permissions p WHERE :username = p.user.username AND p.permissionType = 'WRITE' AND b.archived IS NULL",
         Board.class);
 
     query.setParameter("username", username);
-=======
-  @Override
-  public Iterable<Board> listActiveBoardsThatUserCanWrite(SystemUser user) {
-    TypedQuery<Board> query = createQuery(
-        "SELECT DISTINCT b FROM Board b LEFT JOIN FETCH b.permissions WHERE b.owner = :user OR b.permissions IN (SELECT p FROM UserPermission p WHERE p.user = :user AND p.type = 'WRITE')",
-        Board.class);
-
-    query.setParameter("user", user);
->>>>>>> f636561 (feat: jpa repository listActiveBoardsThatUserCanWrite (#50))
 
     return query.getResultList();
   }
 
-  // we are using this function to send the boards to the shared board app when the user asks for
-  // the boards he owns. we send a list of BoardDTO serialized - the problem is that when
-  // retrieving the boards from the database, jpa creates a proxy for the Board entity (the
-  // default behavior for loading lists is lazy loading which we don't want here since the client
-  // does not have access to the database). we could have made the default behavior eager loading
-  // but that would impact the performance of the app, so we will force the loading of the lists
+  // we are using this function to send the boards to the shared board app when
+  // the user asks for
+  // the boards he owns. we send a list of BoardDTO serialized - the problem is
+  // that when
+  // retrieving the boards from the database, jpa creates a proxy for the Board
+  // entity (the
+  // default behavior for loading lists is lazy loading which we don't want here
+  // since the client
+  // does not have access to the database). we could have made the default
+  // behavior eager loading
+  // but that would impact the performance of the app, so we will force the
+  // loading of the lists
   // here.
 
   // Reference: https://stackoverflow.com/a/51055523/15339625
 
-  // SELECT DISTINCT b FROM Board b LEFT JOIN FETCH b.permissions WHERE b.owner.username = :username
+  // SELECT DISTINCT b FROM Board b LEFT JOIN FETCH b.permissions WHERE
+  // b.owner.username = :username
   // SELECT DISTINCT b FROM Board b LEFT JOIN FETCH b.rows WHERE b in :boards
   // SELECT DISTINCT b FROM Board b LEFT JOIN FETCH b.columns WHERE b in :boards
 
