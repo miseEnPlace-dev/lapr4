30a8181d10ecb3da85ae0839482b335e15693a0c
Carlos Lopes <1211277@isep.ipp.pt>
2023-06-11 17:09:41 +0100

    feat: board service (#50)

---
+++ ecourse.core/src/main/java/eapli/ecourse/postitmanagement/application/BoardService.java
@@ -0,0 +1,38 @@
+package eapli.ecourse.postitmanagement.application;
+
+import eapli.ecourse.boardmanagement.domain.BoardID;
+import eapli.ecourse.boardmanagement.repositories.BoardRepository;
+import eapli.ecourse.postitmanagement.domain.Coordinates;
+import eapli.ecourse.postitmanagement.domain.PostIt;
+import eapli.ecourse.postitmanagement.repositories.PostItRepository;
+
+public class BoardService {
+
+  BoardRepository boardRepository;
+  PostItRepository postItRepository;
+
+  public BoardService(BoardRepository boardRepository, PostItRepository postItRepository) {
+    this.boardRepository = boardRepository;
+    this.postItRepository = postItRepository;
+  }
+
+  public boolean isCellAvailable(BoardID id, int x, int y) {
+    Iterable<PostIt> postIts = postItRepository.findAllByBoardId(id);
+
+    postIts = postItRepository.findLatestVersionOfBoard(id);
+
+    return !this.existsPostIt(postIts, x, y);
+  }
+
+  private boolean existsPostIt(Iterable<PostIt> postIts, int x, int y) {
+    Coordinates c = Coordinates.valueOf(x, y);
+
+    for (PostIt p : postIts) {
+      if (p.coordinates().equals(c))
+        return true;
+    }
+
+    return false;
+  }
+
+}
