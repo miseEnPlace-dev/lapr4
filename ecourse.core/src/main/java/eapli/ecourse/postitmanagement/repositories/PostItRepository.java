15f26f18df4638f1d6a2c84213a045444fd5c05f
Carlos Lopes <1211277@isep.ipp.pt>
2023-06-13 18:06:19 +0100

    feat: updatablePostIts method repo (#64)

--- ecourse.core/src/main/java/eapli/ecourse/postitmanagement/repositories/PostItRepository.java
+++ ecourse.core/src/main/java/eapli/ecourse/postitmanagement/repositories/PostItRepository.java
@@ -4,11 +4,14 @@ import eapli.ecourse.boardmanagement.domain.BoardID;
 import eapli.ecourse.postitmanagement.domain.PostIt;
 import eapli.ecourse.postitmanagement.domain.PostItID;
 import eapli.framework.domain.repositories.DomainRepository;
+import eapli.framework.infrastructure.authz.domain.model.Username;
 
 public interface PostItRepository extends DomainRepository<PostItID, PostIt> {
 
   public Iterable<PostIt> findAllByBoardId(BoardID boardId);
 
   public Iterable<PostIt> findLatestVersionOfBoard(BoardID boardId);
+
+  public Iterable<PostIt> findLatestPostItsCreatedByUser(BoardID boardId, Username username);
 
 }
