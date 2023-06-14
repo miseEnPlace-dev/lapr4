afe757789846aa3350be4e0581a32f06ff9e65ac
André Barros <1211299@isep.ipp.pt>
2023-06-14 17:36:27 +0100

    fix: formative exam request tests (#53)

--- ecourse.core/src/test/java/eapli/ecourse/exammanagement/domain/FormativeExamRequestTest.java
+++ ecourse.core/src/test/java/eapli/ecourse/exammanagement/domain/FormativeExamRequestTest.java
@@ -1,101 +1,99 @@
-// package eapli.ecourse.exammanagement.domain;
+package eapli.ecourse.exammanagement.domain;
 
-// import static org.junit.Assert.assertEquals;
-// import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-// import org.junit.Before;
-// import org.junit.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-// import eapli.ecourse.exammanagement.domain.evaluation.ExamScore;
-// import eapli.ecourse.exammanagement.domain.formative.FormativeExamRequest;
-// import
-// eapli.ecourse.exammanagement.domain.formative.FormativeExamRequestBuilder;
-// import
-// eapli.ecourse.exammanagement.domain.formative.FormativeExamSectionRequest;
+import eapli.ecourse.exammanagement.domain.evaluation.ExamScore;
+import eapli.ecourse.exammanagement.domain.formative.FormativeExamRequest;
+import eapli.ecourse.exammanagement.domain.formative.FormativeExamRequestBuilder;
+import eapli.ecourse.exammanagement.domain.formative.FormativeExamSectionRequest;
 
-// public class FormativeExamRequestTest {
-// private FormativeExamRequest request;
-// private ExamIdentifier identifier;
-// private ExamTitle title;
-// private ExamDescription description;
-// private ExamScore score;
-// private List<FormativeExamSectionRequest> sections;
+public class FormativeExamRequestTest {
+  private FormativeExamRequest request;
+  private ExamIdentifier identifier;
+  private ExamTitle title;
+  private ExamDescription description;
+  private ExamScore score;
+  private List<FormativeExamSectionRequest> sections;
+  private ExamInfo gradeInfo;
 
-// @Before
-// public void setUp() {
-// identifier = ExamIdentifier.valueOf("1");
-// title = ExamTitle.valueOf("Test Exam");
-// description = ExamDescription.valueOf("This is a test exam");
-// sections = new ArrayList<>();
-// SectionIdentifier sectionIdentifier = SectionIdentifier.valueOf("1");
-// SectionTitle sectionTitle = SectionTitle.valueOf("Test Section");
-// SectionDescription sectionDescription = SectionDescription.valueOf("This is a
-// test section");
-// Integer numberOfQuestions = 10;
-// String questionsType = "Multiple Choice";
-// score = ExamScore.valueOf(12d);
-// FormativeExamSectionRequest sectionRequest = new
-// FormativeExamSectionRequest(sectionIdentifier, sectionTitle,
-// sectionDescription,
-// numberOfQuestions, questionsType);
-// sections.add(sectionRequest);
-// request = new FormativeExamRequest(identifier, title, description, score,
-// sections);
-// }
+  @Before
+  public void setUp() {
+    identifier = ExamIdentifier.valueOf("1");
+    title = ExamTitle.valueOf("Test Exam");
+    description = ExamDescription.valueOf("This is a test exam");
+    sections = new ArrayList<>();
+    SectionIdentifier sectionIdentifier = SectionIdentifier.valueOf("1");
+    SectionTitle sectionTitle = SectionTitle.valueOf("Test Section");
+    SectionDescription sectionDescription = SectionDescription.valueOf("This is a test section");
+    Integer numberOfQuestions = 10;
+    String questionsType = "Multiple Choice";
+    score = ExamScore.valueOf(12d);
+    FormativeExamSectionRequest sectionRequest = new FormativeExamSectionRequest(sectionIdentifier, sectionTitle,
+        sectionDescription,
+        numberOfQuestions, questionsType);
+    sections.add(sectionRequest);
+    gradeInfo = (ExamInfo.ON_SUBMIT);
+    request = new FormativeExamRequest(identifier, title, description, score,
+        sections, gradeInfo);
+  }
 
-// @Test
-// public void testIdentifier() {
-// assertEquals(identifier, request.identifier());
-// }
+  @Test
+  public void testIdentifier() {
+    assertEquals(identifier, request.identifier());
+  }
 
-// @Test
-// public void testTitle() {
-// assertEquals(title, request.title());
-// }
+  @Test
+  public void testTitle() {
+    assertEquals(title, request.title());
+  }
 
-// @Test
-// public void testDescription() {
-// assertEquals(description, request.description());
-// }
+  @Test
+  public void testDescription() {
+    assertEquals(description, request.description());
+  }
 
-// @Test
-// public void testSections() {
-// assertEquals(sections, request.sections());
-// }
+  @Test
+  public void testSections() {
+    assertEquals(sections, request.sections());
+  }
 
-// // @Test
-// // public void testSameAs() {
-// // FormativeExamRequest other = new FormativeExamRequest(identifier, title,
-// // description, score, sections);
-// // assertTrue(request.sameAs(other));
-// // }
+  @Test
+  public void testSameAs() {
+    FormativeExamRequest other = new FormativeExamRequest(identifier, title,
+        description, score, sections, gradeInfo);
+    assertTrue(request.sameAs(other));
+  }
 
-// // @Test
-// // public void testEquals() {
-// // FormativeExamRequest other = new FormativeExamRequest(identifier, title,
-// // description, score, sections);
-// // assertEquals(request, other);
-// // }
+  @Test
+  public void testEquals() {
+    FormativeExamRequest other = new FormativeExamRequest(identifier, title,
+        description, score, sections, gradeInfo);
+    assertEquals(request, other);
+  }
 
-// // @Test
-// // public void testHashCode() {
-// // FormativeExamRequest other = new FormativeExamRequest(identifier, title,
-// // description, score, sections);
-// // assertEquals(request.hashCode(), other.hashCode());
-// // }
+  @Test
+  public void testHashCode() {
+    FormativeExamRequest other = new FormativeExamRequest(identifier, title,
+        description, score, sections, gradeInfo);
+    assertEquals(request.hashCode(), other.hashCode());
+  }
 
-// // @Test
-// // public void testBuilder() {
-// // FormativeExamRequestBuilder builder = new FormativeExamRequestBuilder();
-// // builder.withIdentifier(identifier);
-// // builder.withTitle(title);
-// // builder.withDescription(description);
-// // builder.withScore(score);
-// // builder.withSections(sections);
-// // FormativeExamRequest other = builder.build();
-// // assertEquals(request, other);
-// // }
-// }
+  @Test
+  public void testBuilder() {
+    FormativeExamRequestBuilder builder = new FormativeExamRequestBuilder();
+    builder.withIdentifier(identifier);
+    builder.withTitle(title);
+    builder.withDescription(description);
+    builder.withScore(score);
+    builder.withSections(sections);
+    FormativeExamRequest other = builder.build();
+    assertEquals(request, other);
+  }
+}
