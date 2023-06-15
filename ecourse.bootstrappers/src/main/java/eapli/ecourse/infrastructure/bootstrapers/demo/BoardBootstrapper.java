9d8776e6c1255e0b859454dd72877b1c6721b82b
Tomás Lopes <1211289@isep.ipp.pt>
2023-06-15 11:18:28 +0100

    fix: fixed board bootstrap

--- ecourse.bootstrappers/src/main/java/eapli/ecourse/infrastructure/bootstrapers/demo/BoardBootstrapper.java
+++ ecourse.bootstrappers/src/main/java/eapli/ecourse/infrastructure/bootstrapers/demo/BoardBootstrapper.java
@@ -1,12 +1,13 @@
 package eapli.ecourse.infrastructure.bootstrapers.demo;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import eapli.ecourse.boardmanagement.application.CreateBoardController;
 import eapli.ecourse.boardmanagement.domain.Board;
 import eapli.ecourse.boardmanagement.domain.PermissionType;
@@ -23,11 +24,10 @@ import eapli.framework.infrastructure.authz.domain.model.SystemUser;
 public class BoardBootstrapper extends UsersBootstrapperBase implements Action {
   private static Logger logger = LogManager.getLogger(BoardBootstrapper.class);
 
-  private final String IMAGE_PATH = "p1.png";
+  private final String IMAGE_PATH = "ecourse.bootstrappers/src/main/resources/_p1.png";
 
-  private CreateBoardController ctrl =
-      new CreateBoardController(PersistenceContext.repositories().boards(),
-          AuthzRegistry.userService(), AuthzRegistry.authorizationService());
+  private CreateBoardController ctrl = new CreateBoardController(PersistenceContext.repositories().boards(),
+      AuthzRegistry.userService(), AuthzRegistry.authorizationService());
 
   private CreatePostItController ctrlPostIt = new CreatePostItController(
       PersistenceContext.repositories().boards(), PersistenceContext.repositories().postIts(),
@@ -67,36 +67,30 @@ public class BoardBootstrapper extends UsersBootstrapperBase implements Action {
 
   @Override
   public boolean execute() {
-
     Board b = ctrl.createBoard("example", getPermissions(), getColumns(), getRows());
 
     try {
-      String path = null;
-
-      ClassLoader classLoader = getClass().getClassLoader();
-      URL resource = classLoader.getResource(IMAGE_PATH);
-      if (resource != null)
-        path = resource.getPath();
-      else
-        logger.warn("File {} not found", IMAGE_PATH);
-
-      ctrlPostIt.createPostIt(b.identity(), 1, 2, "PostIt1", null, path);
+      ctrlPostIt.createPostIt(b.identity(), 1, 2, "PostIt1", null, IMAGE_PATH);
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-    PostIt p2 = null;
     try {
-      p2 = ctrlPostIt.createPostIt(b.identity(), 2, 2, "PostIt2", "Description", null);
+      ctrlPostIt.createPostIt(b.identity(), 2, 3, "PostIt4", "Some very big descriptive description!", IMAGE_PATH);
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-    ChangePostItController c =
-        new ChangePostItController(PersistenceContext.repositories().boards(),
-            PersistenceContext.repositories().postIts(), AuthzRegistry.authorizationService());
+    try {
+      PostIt p2 = ctrlPostIt.createPostIt(b.identity(), 2, 2, "PostIt2", "Description", null);
 
-    c.changePostIt(p2.identity(), "PostIt2.1", 2, 2, "Description updated", null);
+      ChangePostItController c = new ChangePostItController(PersistenceContext.repositories().boards(),
+          PersistenceContext.repositories().postIts(), AuthzRegistry.authorizationService());
+
+      c.changePostIt(p2.identity(), "PostIt2.1", 2, 2, "Description updated", null);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
     return true;
   }
