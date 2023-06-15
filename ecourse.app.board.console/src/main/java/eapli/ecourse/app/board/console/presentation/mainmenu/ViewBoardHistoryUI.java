bfc3d3e0964c306d75b949f9a009721cb0db183c
Carlos Lopes <1211277@isep.ipp.pt>
2023-06-15 16:37:50 +0100

    chore: removed useless imports (#64)

--- ecourse.app.board.console/src/main/java/eapli/ecourse/app/board/console/presentation/mainmenu/ViewBoardHistoryUI.java
+++ ecourse.app.board.console/src/main/java/eapli/ecourse/app/board/console/presentation/mainmenu/ViewBoardHistoryUI.java
@@ -1,7 +1,6 @@
 package eapli.ecourse.app.board.console.presentation.mainmenu;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
