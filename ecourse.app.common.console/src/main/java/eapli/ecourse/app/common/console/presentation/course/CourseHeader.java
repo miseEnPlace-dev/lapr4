d27a4fc23306d36a6fe3a1354842a5299a628d66
André Barros <1211299@isep.ipp.pt>
2023-06-15 12:37:02 +0100

    fix: course header

--- ecourse.app.common.console/src/main/java/eapli/ecourse/app/common/console/presentation/course/CourseHeader.java
+++ ecourse.app.common.console/src/main/java/eapli/ecourse/app/common/console/presentation/course/CourseHeader.java
@@ -2,7 +2,7 @@ package eapli.ecourse.app.common.console.presentation.course;
 
 public class CourseHeader {
   public String header() {
-    return String.format("#  %-7s%-20s%-30s%-5s%-5s%-16s%-16s%-20s", "Code", "Title", "Description", "Min", "Max",
+    return String.format("\n#  %-7s%-20s%-30s%-5s%-5s%-16s%-16s%-20s", "Code", "Title", "Description", "Min", "Max",
         "State",
         "Enrolment State", "Created At");
   }
