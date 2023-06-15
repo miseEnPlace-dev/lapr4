c01ab53b9e46f39d3862efee6a5e1fe5a28d7005
Ricardo M <rmoreiradev@gmail.com>
2023-06-15 23:07:32 +0100

    refactor: updated share board controller
    Refs: #58

---
+++ ecourse.core/src/main/java/eapli/ecourse/boardmanagement/application/ShareBoardController.java
@@ -0,0 +1,90 @@
+package eapli.ecourse.boardmanagement.application;
+
+import java.util.Calendar;
+import java.util.stream.StreamSupport;
+import eapli.ecourse.boardmanagement.domain.Board;
+import eapli.ecourse.boardmanagement.domain.BoardID;
+import eapli.ecourse.boardmanagement.domain.PermissionType;
+import eapli.ecourse.boardmanagement.domain.UserPermission;
+import eapli.ecourse.boardmanagement.dto.UserPermissionDTO;
+import eapli.ecourse.boardmanagement.repositories.BoardRepository;
+import eapli.framework.application.UseCaseController;
+import eapli.framework.infrastructure.authz.application.UserManagementService;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+import eapli.framework.infrastructure.authz.domain.model.Username;
+
+@UseCaseController
+public class ShareBoardController {
+  private final BoardRepository boardRepository;
+  private final UserManagementService userSvc;
+
+  public ShareBoardController(BoardRepository boardRepository, UserManagementService userSvc) {
+    this.boardRepository = boardRepository;
+    this.userSvc = userSvc;
+  }
+
+  public boolean boardExists(BoardID boardId) {
+    return boardRepository.ofIdentity(boardId).isPresent();
+  }
+
+  public boolean isBoardParticipant(BoardID boardId, Username username) {
+    Board board = boardRepository.ofIdentity(boardId).orElseThrow();
+    return board.participates(username);
+  }
+
+  public boolean isBoardOwner(BoardID boardId, Username username) {
+    Board board = boardRepository.ofIdentity(boardId).orElseThrow();
+    return board.owner().hasIdentity(username);
+  }
+
+  public UserPermissionDTO getUserPermission(BoardID boardId, Username username) {
+    Board board = boardRepository.ofIdentity(boardId).orElseThrow();
+
+    UserPermission userPermission = StreamSupport.stream(board.permissions().spliterator(), true)
+        .filter(p -> p.user().hasIdentity(username)).findFirst().orElse(null);
+
+    return userPermission == null ? null : userPermission.toDto();
+  }
+
+  public UserPermissionDTO addPermission(BoardID boardId, Username username,
+      PermissionType permissionType) {
+    Board board = boardRepository.ofIdentity(boardId).orElseThrow();
+    Calendar createdAt = Calendar.getInstance();
+    SystemUser user = userSvc.userOfIdentity(username).orElseThrow();
+    UserPermission userPermission = new UserPermission(createdAt, createdAt, permissionType, user);
+    board.permissions().add(userPermission);
+    boardRepository.save(board);
+    return userPermission.toDto();
+  }
+
+  public UserPermissionDTO updatePermission(BoardID boardId, Username username,
+      PermissionType permissionType) {
+    Board board = boardRepository.ofIdentity(boardId).orElseThrow();
+
+    UserPermission userPermission = StreamSupport.stream(board.permissions().spliterator(), true)
+        .filter(p -> p.user().hasIdentity(username)).findFirst().orElseThrow();
+
+    // modify the existing permission
+    if (userPermission.permissionType().isRead() && permissionType.isWrite())
+      userPermission.permissionType().changeToWrite();
+    else if (userPermission.permissionType().isWrite() && permissionType.isRead())
+      userPermission.permissionType().changeToRead();
+
+    boardRepository.save(board);
+
+    return userPermission.toDto();
+  }
+
+  public void removePermission(BoardID boardId, Username username) {
+    Board board = boardRepository.ofIdentity(boardId).orElseThrow();
+
+    UserPermission userPermission = StreamSupport.stream(board.permissions().spliterator(), true)
+        .filter(p -> p.user().hasIdentity(username)).findFirst().orElse(null);
+
+    if (userPermission == null)
+      return;
+
+    board.permissions().remove(userPermission);
+    board = boardRepository.save(board);
+  }
+}
--- ecourse.daemon.board/src/main/java/eapli/ecourse/daemon/board/messages/GetUserPermissionsMessage.java
+++ ecourse.daemon.board/src/main/java/eapli/ecourse/daemon/board/messages/GetUserPermissionsMessage.java
@@ -4,13 +4,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Optional;
-import java.util.stream.StreamSupport;
 
 import javax.json.JsonObject;
-
-import eapli.ecourse.boardmanagement.domain.Board;
+import eapli.ecourse.boardmanagement.application.ShareBoardController;
 import eapli.ecourse.boardmanagement.domain.BoardID;
-import eapli.ecourse.boardmanagement.domain.UserPermission;
 import eapli.ecourse.boardmanagement.dto.UserPermissionDTO;
 import eapli.ecourse.boardmanagement.repositories.BoardRepository;
 import eapli.ecourse.common.board.SafeOnlineCounter;
@@ -29,6 +26,7 @@ import eapli.framework.infrastructure.authz.domain.model.Username;
 public class GetUserPermissionsMessage extends Message {
   private final BoardRepository boardRepository;
   private final UserManagementService userSvc;
+  private final ShareBoardController ctrl;
 
   public GetUserPermissionsMessage(ProtocolMessage protocolMessage, DataOutputStream output,
       Socket socket, SafeOnlineCounter onlineCounter) {
@@ -36,6 +34,7 @@ public class GetUserPermissionsMessage extends Message {
 
     this.boardRepository = PersistenceContext.repositories().boards();
     this.userSvc = AuthzRegistry.userService();
+    this.ctrl = new ShareBoardController(boardRepository, userSvc);
   }
 
   @Override
@@ -48,47 +47,48 @@ public class GetUserPermissionsMessage extends Message {
 
     JsonObject json = protocolMessage.getPayloadAsJson();
 
-    String boardId = json.getString("boardId");
-    String username = json.getString("username");
+    String boardIdStr = json.getString("boardId");
+    String usernameStr = json.getString("username");
 
-    if (boardId == null || username == null) {
+    if (boardIdStr == null || usernameStr == null) {
       send(new ProtocolMessage(MessageCode.ERR, "Bad Request"));
       return;
     }
 
-    Optional<Board> board = boardRepository.ofIdentity(BoardID.valueOf(boardId));
+    BoardID boardId = BoardID.valueOf(boardIdStr);
+    Username authUsername =
+        Username.valueOf(clientState.getCredentialStore().getUser().getUsername());
 
-    if (!board.isPresent()) {
+    // verify if the board with the given id exists
+    if (!ctrl.boardExists(boardId)) {
       send(new ProtocolMessage(MessageCode.ERR, "Board not found"));
       return;
     }
 
-    // only the board owner can get the permissions of other users
-    Username authenticatedUsername =
-        Username.valueOf(clientState.getCredentialStore().getUser().getUsername());
-
-    if (!board.get().participates(authenticatedUsername)) {
+    // check if the authenticated user has read permission
+    if (!ctrl.isBoardParticipant(boardId, authUsername)) {
       send(new ProtocolMessage(MessageCode.ERR, "Unauthorized"));
       return;
     }
 
-    Optional<SystemUser> user = userSvc.userOfIdentity(Username.valueOf(username));
+    // verify if the user with the given username exists
+    Optional<SystemUser> user = userSvc.userOfIdentity(Username.valueOf(usernameStr));
 
     if (!user.isPresent()) {
       send(new ProtocolMessage(MessageCode.ERR, "User not found"));
       return;
     }
 
-    UserPermission permission = StreamSupport.stream(board.get().permissions().spliterator(), true)
-        .filter(p -> p.user().equals(user.get())).findFirst().orElse(null);
+    Username username = Username.valueOf(usernameStr);
 
-    if (permission == null) {
-      send(new ProtocolMessage(MessageCode.GET_USER_PERMISSIONS));
+    // check if the user is owner of the board
+    if (ctrl.isBoardOwner(boardId, username)) {
+      send(new ProtocolMessage(MessageCode.ERR, "User is the owner of the board"));
       return;
     }
 
-    UserPermissionDTO permissionDTO = permission.toDto();
+    UserPermissionDTO userPermission = ctrl.getUserPermission(boardId, username);
 
-    send(new ProtocolMessage(MessageCode.GET_USER_PERMISSIONS, permissionDTO));
+    send(new ProtocolMessage(MessageCode.GET_USER_PERMISSIONS, userPermission));
   }
 }
