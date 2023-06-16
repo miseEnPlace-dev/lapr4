8e16061c6938749884fa351cbe110b4d18884c4b
André Barros <1211299@isep.ipp.pt>
2023-06-16 21:16:05 +0100

    fix: safe online counter bug

--- ecourse.common.board/src/main/java/eapli/ecourse/common/board/TcpServer.java
+++ ecourse.common.board/src/main/java/eapli/ecourse/common/board/TcpServer.java
@@ -12,13 +12,13 @@ public class TcpServer {
   private Class<? extends Runnable> handlerClass;
   private boolean secure;
   private SafeOnlineCounter onlineCounter;
-  private OnlineSafeShared shared = new OnlineSafeShared();
+  private OnlineSafeShared onlineShared = new OnlineSafeShared();
 
   public TcpServer(int port, Class<? extends Runnable> handler, boolean secure) {
     this.port = port;
     this.handlerClass = handler;
     this.secure = secure;
-    this.onlineCounter = new SafeOnlineCounter(shared);
+    this.onlineCounter = new SafeOnlineCounter(onlineShared);
   }
 
   public void run() {
@@ -41,7 +41,7 @@ public class TcpServer {
 
     System.out.printf("[TCP%s Server] Listening on port %d!\n", this.secure ? " SSL" : "", port);
 
-    Thread onlineCounterThread = new Thread(new OnlineCheckerHandler(shared));
+    Thread onlineCounterThread = new Thread(new OnlineCheckerHandler(onlineShared));
     onlineCounterThread.start();
 
     while (!tcpSocket.isClosed()) {
