3a9a2f6b9e1a2e799f3281bad9d45eaa4b7c834c
Ricardo M <rmoreiradev@gmail.com>
2023-06-16 17:06:42 +0100

    fix: fixed undo post-it test

--- ecourse.core/src/test/java/eapli/ecourse/postitmanagement/application/UndoPostItControllerTest.java
+++ ecourse.core/src/test/java/eapli/ecourse/postitmanagement/application/UndoPostItControllerTest.java
@@ -1,44 +1,57 @@
 package eapli.ecourse.postitmanagement.application;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import eapli.ecourse.boardmanagement.domain.Board;
+import eapli.ecourse.boardmanagement.repositories.BoardRepository;
 import eapli.ecourse.postitmanagement.domain.PostIt;
 import eapli.ecourse.postitmanagement.repositories.PostItRepository;
+import eapli.framework.domain.repositories.TransactionalContext;
+import eapli.framework.infrastructure.authz.application.UserManagementService;
 import eapli.framework.infrastructure.authz.domain.model.SystemUser;
 
 public class UndoPostItControllerTest extends PostItControllerBaseTest {
+  private BoardRepository boardRepository;
   private PostItRepository postItRepository;
+  private UndoPostItController ctrl;
+  private UserManagementService usrSvc;
 
   @Before
   public void setup() {
-    new UndoPostItController(null, postItRepository);
+    this.boardRepository = mock(BoardRepository.class);
+    this.postItRepository = mock(PostItRepository.class);
+    this.usrSvc = mock(UserManagementService.class);
+
+    TransactionalContext tx = mock(TransactionalContext.class);
+
+    this.ctrl = new UndoPostItController(tx, postItRepository);
   }
 
   @Test
   public void testPostItExists() {
     PostIt postIt = getDummyPostIt();
 
-    // when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.of(postIt));
+    when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.of(postIt));
 
-    // boolean result = ctrl.postItExists(postIt.identity());
+    boolean result = ctrl.postItExists(postIt.identity());
 
-    assertNotNull(postIt);
+    assertTrue(result);
   }
 
   @Test
   public void testPostItDoesNotExist() {
-    PostIt postIt = null;
+    PostIt postIt = getDummyPostIt();
 
-    // when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.empty());
+    when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.empty());
 
-    // boolean result = ctrl.postItExists(postIt.identity());
+    boolean result = ctrl.postItExists(postIt.identity());
 
-    assertNull(postIt);
+    assertFalse(result);
   }
 
   @Test
@@ -46,65 +59,48 @@ public class UndoPostItControllerTest extends PostItControllerBaseTest {
     PostIt postIt = getDummyPostIt();
     SystemUser user = getNewDummyUser();
     assertTrue(postIt.owner().hasIdentity(user.username()));
-    /*
-     * when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.of(postIt));
-     * when(usrSvc.activateUser(user)).thenReturn(user);
-     *
-     * when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.of(postIt));
-     *
-     * boolean result = ctrl.canEditPostIt(postIt.identity(), user.username());
-     *
-     * assertTrue(result);
-     */
+
+    when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.of(postIt));
+    when(usrSvc.activateUser(user)).thenReturn(user);
+
+    when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.of(postIt));
+
+    boolean result = ctrl.canEditPostIt(postIt.identity(), user.username());
+
+    assertTrue(result);
   }
 
   @Test
   public void testCanEditPostItAsBoardOwner() {
-    PostIt postIt = getDummyPostIt();
+    PostIt postIt = getDummyPostIt(); // owner: user1
+
     SystemUser user = getNewDummyUser();
-    assertTrue(postIt.owner().hasIdentity(user.username()));
 
     Board board = getDummyBoard();
-    // user is owner of board
 
-    PostIt postIt2 = getDummyPostIt2();
-    assertTrue(postIt.owner().hasIdentity(user.username()));
-    assertFalse(postIt2.owner().hasIdentity(user.username()));
-    assertTrue(board.owner().hasIdentity(postIt.owner().identity()));
-    /*
-     * when(postItRepository.ofIdentity(postItId)).thenReturn(Optional.of(postIt));
-     * when(boardRepository.ofIdentity(postIt.board())).thenReturn(Optional.of(board));
-     * when(userSvc.sessionUser()).thenReturn(new SystemUser(username));
-     *
-     * boolean result = ctrl.canEditPostIt(postItId, username);
-     *
-     * assertTrue(result);
-     */
+    when(boardRepository.ofIdentity(postIt.board().identity())).thenReturn(Optional.of(board));
+    when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.of(postIt));
+    when(usrSvc.activateUser(user)).thenReturn(user);
+
+    boolean result = ctrl.canEditPostIt(postIt.identity(), user.username());
+
+    assertTrue(result);
   }
 
   @Test
   public void testCannotEditPostIt() {
-    PostIt postIt = getDummyPostIt();
+    PostIt postIt = getDummyPostIt2(); // owner: user2
+
     SystemUser user = getNewDummyUser();
-    assertTrue(postIt.owner().hasIdentity(user.username()));
 
     Board board = getDummyBoard();
-    // user is owner of board
 
-    PostIt postIt2 = getDummyPostIt2();
-    assertTrue(postIt.owner().hasIdentity(user.username()));
-    assertFalse(postIt2.owner().hasIdentity(user.username()));
-    assertTrue(board.owner().hasIdentity(postIt.owner().identity()));
+    when(boardRepository.ofIdentity(postIt.board().identity())).thenReturn(Optional.of(board));
+    when(postItRepository.ofIdentity(postIt.identity())).thenReturn(Optional.of(postIt));
+    when(usrSvc.activateUser(user)).thenReturn(user);
 
-    /*
-     * when(postItRepository.ofIdentity(postItId)).thenReturn(Optional.of(postIt));
-     * when(boardRepository.ofIdentity(postIt.board())).thenReturn(Optional.of(board));
-     * when(userSvc.sessionUser()).thenReturn(new SystemUser(username));
-     *
-     * boolean result = ctrl.canEditPostIt(postItId, username);
-     *
-     * assertFalse(result);
-     */
+    boolean result = ctrl.canEditPostIt(postIt.identity(), user.username());
+
+    assertFalse(result);
   }
-
 }
