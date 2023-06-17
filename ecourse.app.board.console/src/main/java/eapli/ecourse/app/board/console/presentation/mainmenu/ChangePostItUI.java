b689d83d1d441ca6f7daf16f7e05b9636ea2a355
Ricardo M <rmoreiradev@gmail.com>
2023-06-17 18:00:47 +0100

    chore: improved ux in board app

--- ecourse.app.board.console/src/main/java/eapli/ecourse/app/board/console/presentation/mainmenu/ChangePostItUI.java
+++ ecourse.app.board.console/src/main/java/eapli/ecourse/app/board/console/presentation/mainmenu/ChangePostItUI.java
@@ -33,63 +33,87 @@ public class ChangePostItUI extends AbstractUI {
         return false;
       }
 
-      BoardPrinter printer = new BoardPrinter();
-      printer.printHeader();
+      BoardDTO selectedBoard = null;
+      PostItDTO selectedPostIt = null;
 
-      SelectWidget<BoardDTO> selector = new SelectWidget<>("", boards, printer);
-      selector.show();
+      do {
+        System.out.println("Boards you can edit:\n");
 
-      final BoardDTO selected = selector.selectedElement();
+        BoardPrinter printer = new BoardPrinter();
+        printer.printHeader();
 
-      // POST_ITS
-      Iterable<PostItDTO> postIts = ctrl.listUserUpdatablePostIts(selected.getId());
+        SelectWidget<BoardDTO> selector = new SelectWidget<>("", boards, printer);
+        selector.show();
 
-      if (!postIts.iterator().hasNext()) {
-        System.out.println("You don't have permission to update any post-it in that board.");
-        return false;
-      }
+        selectedBoard = selector.selectedElement();
 
-      PostItPrinter postItPrinter = new PostItPrinter();
-      postItPrinter.printHeader();
+        if (selectedBoard == null) {
+          System.out.println("\nOperation cancelled by the user.");
+          return false;
+        }
 
-      SelectWidget<PostItDTO> postItSelector = new SelectWidget<>("", postIts, postItPrinter);
-      postItSelector.show();
+        // POST_ITS
+        Iterable<PostItDTO> postIts = ctrl.listUserUpdatablePostIts(selectedBoard.getId());
 
-      final PostItDTO selectedPostIt = postItSelector.selectedElement();
+        if (!postIts.iterator().hasNext()) {
+          System.out.println("You don't have permission to update any post-it in that board.");
+          return false;
+        }
+
+        PostItPrinter postItPrinter = new PostItPrinter();
+        postItPrinter.printHeader();
+
+        SelectWidget<PostItDTO> postItSelector = new SelectWidget<>("", postIts, postItPrinter);
+        postItSelector.show();
+
+        selectedPostIt = postItSelector.selectedElement();
+      } while (selectedPostIt == null);
 
       // CHANGE
       Integer x = null, y = null;
       boolean success = true;
 
-      if (Console.readLine("Do you want to change the post-it coordinates? (Y/N): ").equalsIgnoreCase("Y"))
+      if (Console.readLine("Do you want to change the post-it coordinates? (Y/n): ")
+          .equalsIgnoreCase("n"))
         do {
           System.out.println("\nWrite the new post-it coordinates:");
           x = Console.readInteger("X: ");
           y = Console.readInteger("Y: ");
 
-          success = ctrl.validateCoordinates(selected.getId(), x, y);
+          success = ctrl.validateCoordinates(selectedBoard.getId(), x, y);
           if (!success) {
-            System.out.println("Cell not available. Make sure the cell exists and is free! Try again.");
+            System.out
+                .println("Cell not available. Make sure the cell exists and is free! Try again.");
           }
         } while (!success);
 
       String title = null;
-      if (Console.readLine("Do you want to change the post-it title? (Y/N): ").equalsIgnoreCase("Y"))
+      if (Console.readLine("Do you want to change the post-it title? (Y/n): ")
+          .equalsIgnoreCase("n"))
         title = Console.readLine("Write the post-it title: ");
 
       String description = null;
-      if (Console.readLine("Do you want to change the post-it description? (Y/N): ").equalsIgnoreCase("Y"))
+      if (Console.readLine("Do you want to change the post-it description? (Y/n): ")
+          .equalsIgnoreCase("n"))
         description = Console.readLine("Write the post-it description (Press Enter to delete): ");
 
       String imagePath = null;
-      if (Console.readLine("Do you want to change the post-it image? (Y/N): ").equalsIgnoreCase("Y"))
+      if (Console.readLine("Do you want to change the post-it image? (Y/n): ")
+          .equalsIgnoreCase("n"))
         imagePath = Console.readLine("Write the post-it image path (Press Enter to delete): ");
+
+      if (!Console.readLine("Review the changes. Are you sure you want to edit the post-it \""
+          + selectedPostIt.getTitle() + "\"? (y/N)").equalsIgnoreCase("y")) {
+        System.out.println("\nOperation cancelled by the user.");
+        return false;
+      }
 
       ctrl.changePostIt(selectedPostIt.getId(), x, y, title, description, imagePath);
 
       System.out.println("\nPost-it changed successfully.");
 
-    } catch (ClassNotFoundException | IOException | UnsupportedVersionException | UnsuccessfulRequestException e) {
+    } catch (ClassNotFoundException | IOException | UnsupportedVersionException
+        | UnsuccessfulRequestException e) {
       logger.error("Error trying to view a board", e);
     }
 
