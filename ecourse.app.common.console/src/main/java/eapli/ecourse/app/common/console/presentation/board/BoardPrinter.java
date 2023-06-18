f03973d155edfc048748e7f219be59db3f6b0f5a
André Barros <1211299@isep.ipp.pt>
2023-06-18 16:47:33 +0100

    fix: board printer

--- ecourse.app.common.console/src/main/java/eapli/ecourse/app/common/console/presentation/board/BoardPrinter.java
+++ ecourse.app.common.console/src/main/java/eapli/ecourse/app/common/console/presentation/board/BoardPrinter.java
@@ -5,10 +5,10 @@ import eapli.ecourse.boardmanagement.dto.BoardDTO;
 import eapli.framework.visitor.Visitor;
 
 public class BoardPrinter implements Visitor<BoardDTO> {
-  private String format = "%-38s%-10s%-17s%-12s%-6s%-9s%-17s";
+  private String format = "%-10s%-17s%-12s%-6s%-9s%-17s";
 
   public String header() {
-    return String.format("#  " + format, "ID", "Title", "Archived?", "Owner", "Rows", "Columns",
+    return String.format("#  " + format, "Title", "Archived?", "Owner", "Rows", "Columns",
         "Num. Permissions");
   }
 
@@ -20,7 +20,6 @@ public class BoardPrinter implements Visitor<BoardDTO> {
   public void visit(final BoardDTO visitee) {
     TableFormatPrinter printer = new TableFormatPrinter();
 
-    printer.addColumn(visitee.getId().toString(), 38);
     printer.addColumn(visitee.getTitle().toString(), 10);
     printer.addColumn(visitee.getArchived() == null ? "No" : "Yes", 17);
     printer.addColumn(visitee.getOwner().username().toString(), 12);
